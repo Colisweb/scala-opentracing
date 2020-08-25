6d119cc3946633f46f03cd77f66b1ddb3668e6d5
Cyril Verdier <cyril.verdier@colisweb.com>
2020-08-25 15:37:18 +0200

    wip: Move tracing / logging logic

--- http/server/src/main/scala/com/colisweb/tracing/http/server/TracedRoutes.scala
+++ http/server/src/main/scala/com/colisweb/tracing/http/server/TracedRoutes.scala
@@ -1,5 +1,6 @@
 package com.colisweb.tracing.http.server
 
+import cats.implicits._
 import cats.data._
 import cats.effect._
 import com.colisweb.tracing.core.{TracingContext, TracingContextBuilder}
@@ -41,24 +42,44 @@ trait TracedRoutes {
   ) {
     implicit val logger: Logger = LoggerFactory.getLogger("endpoint." + e.info.name.getOrElse(""))
 
+    // TracedHttpRoutes
+    private def tracingLogicWrapper[F[_]: Sync](req: Request[F], logic: (In, TracingContext[F]) => F[Out]): In => F[Out] = ???
+
+    // LoggedHttpRoutes
+    private def loggingLogicWrapper[F[_]: Sync](req: Request[F], logic: (In, TracingContext[F]) => F[Out]): (In, TracingContext[F]) => F[Out] = ???
+    private def logBadRequest[F[_]: Sync](req: Request[F], res: Response[F]): F[Unit] = ???
+
+
     def toTracedRouteRecoverErrors[F[_]: Sync](logic: (In, TracingContext[F]) => F[Out])(
         implicit
         builder: TracingContextBuilder[F],
         eClassTag: ClassTag[Err],
         cs: ContextShift[F],
         serverOptions: Http4sServerOptions[F]): HttpRoutes[F] =
-      TracedHttpRoutes.wrapHttpRoutes(
-        LoggedHttpRoutes.wrapHttpRoutes(Kleisli[OptionT[F, ?], TracedRequest[F], Response[F]] { req =>
-          e.toRouteRecoverErrors(input => logic(input, req.tracingContext))(
-              serverOptions,
-              implicitly,
-              implicitly,
-              implicitly,
-              implicitly
-            )
-            .run(req.request)
-        }),
-        builder
-      )
+      Kleisli[OptionT[F, ?], Request[F], Response[F]] { req =>
+        e.toRouteRecoverErrors(
+          tracingLogicWrapper(req,
+            loggingLogicWrapper(req, logic)
+          )
+        )(serverOptions, implicitly, implicitly, implicitly, implicitly).run(req).flatTransform {
+          case Some(res) if res.status == Status.BadRequest =>
+            logBadRequest(req, res) *> Sync[F].pure[Option[Response[F]]](Some(res))
+          case other => Sync[F].pure[Option[Response[F]]](other)
+        }
+//
+//      TracedHttpRoutes.wrapHttpRoutes(
+//        LoggedHttpRoutes.wrapHttpRoutes(Kleisli[OptionT[F, ?], TracedRequest[F], Response[F]] { req =>
+//          e.toRouteRecoverErrors(input => logic(input, req.tracingContext))(
+//              serverOptions,
+//              implicitly,
+//              implicitly,
+//              implicitly,
+//              implicitly
+//            )
+//            .run(req.request)
+//        }),
+//        builder
+//      )
+    }
   }
 }
