6cf74f2f53f0aa7eb73a173d200f3f82d3681dab
Cyril Verdier <cyril.verdier@colisweb.com>
2021-02-24 17:57:44 +0100

    improve api

--- cats-instrumented/src/main/scala/com/colisweb/tracing/cats/instrumented/Tracing.scala
+++ cats-instrumented/src/main/scala/com/colisweb/tracing/cats/instrumented/Tracing.scala
@@ -2,16 +2,25 @@ package com.colisweb.tracing.cats.instrumented
 
 import cats.effect.{Bracket, ExitCase}
 import io.opentracing.Tracer
+import io.opentracing.Tracer.SpanBuilder
 import io.opentracing.util.GlobalTracer
 
 trait Tracing[F[_]] {
-  val serviceName: String
 
   private val tracer: Tracer = GlobalTracer.get()
 
-  protected def span[A](name: String)(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] = {
+  protected def span[A](name: String)(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] =
+    span(name, identity[SpanBuilder](_))(computation)
+
+  protected def span[A](name: String, serviceName: String)(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] =
+    span(name, _.withTag("service", serviceName))(computation)
+
+  protected def span[A](name: String, serviceName: String, customize: SpanBuilder => SpanBuilder)(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] =
+    span(name, builder => customize(builder.withTag("service", serviceName)))(computation)
+
+  protected def span[A](name: String, customize: SpanBuilder => SpanBuilder)(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] = {
     F.bracketCase(
-      F.catchNonFatal(tracer.buildSpan(name).withTag("service", serviceName).start())
+      F.catchNonFatal(customize(tracer.buildSpan(name)).start())
     ) { currentSpan =>
       tracer.scopeManager().activate(currentSpan)
       computation
