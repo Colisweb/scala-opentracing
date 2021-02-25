f4e54e299ad7207f8b81953279c03b743aa3c4fa
Cyril Verdier <cyril.verdier@colisweb.com>
2021-02-25 10:11:55 +0100

    add rootSpans

--- cats-instrumented/src/main/scala/com/colisweb/tracing/cats/instrumented/Tracing.scala
+++ cats-instrumented/src/main/scala/com/colisweb/tracing/cats/instrumented/Tracing.scala
@@ -9,18 +9,26 @@ trait Tracing[F[_]] {
 
   private val tracer: Tracer = GlobalTracer.get()
 
-  protected def span[A](name: String)(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] =
-    span(name, identity[SpanBuilder](_))(computation)
-
   protected def span[A](name: String, serviceName: String)(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] =
-    span(name, _.withTag("service", serviceName))(computation)
+    span(name, serviceName, identity[SpanBuilder](_))(computation)
 
   protected def span[A](name: String, serviceName: String, customize: SpanBuilder => SpanBuilder)(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] =
     span(name, builder => customize(builder.withTag("service", serviceName)))(computation)
 
-  protected def span[A](name: String, customize: SpanBuilder => SpanBuilder)(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] = {
+  protected def rootSpan[A](name: String, serviceName: String)(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] =
+    rootSpan(name, serviceName, identity[SpanBuilder](_))(computation)
+
+  protected def rootSpan[A](name: String, serviceName: String, customize: SpanBuilder => SpanBuilder)(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] =
+    rootSpan(name, builder => customize(builder.withTag("service", serviceName)))(computation)
+
+  protected def rootSpan[A](name: String, customize: SpanBuilder => SpanBuilder = identity[SpanBuilder](_))(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] =
+    span(name, customize.andThen(_.ignoreActiveSpan()))(computation)
+
+  protected def span[A](name: String, customize: SpanBuilder => SpanBuilder = identity[SpanBuilder](_))(computation: => F[A])(implicit F: Bracket[F, Throwable]): F[A] = {
     F.bracketCase(
-      F.catchNonFatal(customize(tracer.buildSpan(name)).start())
+      F.catchNonFatal(
+        customize(tracer.buildSpan(name)).start()
+      )
     ) { currentSpan =>
       tracer.scopeManager().activate(currentSpan)
       computation
