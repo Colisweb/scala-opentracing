8738d9e8e3f2ee5b97515c6e2b2a33e3948e6e2f
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2021-02-28 16:25:51 +0530

    Fixes #9 Upgrade to cats effect 3

--- context/src/main/scala/com/colisweb/tracing/context/LoggingTracingContext.scala
+++ context/src/main/scala/com/colisweb/tracing/context/LoggingTracingContext.scala
@@ -2,20 +2,18 @@ package com.colisweb.tracing.context
 
 import cats.data.OptionT
 import cats.effect._
-import cats.effect.concurrent.Ref
+import cats.effect.kernel.Ref
 import cats.syntax.all._
 import com.colisweb.tracing.core._
 import com.typesafe.scalalogging.StrictLogging
 import org.slf4j.Logger
-
-import scala.concurrent.duration.MILLISECONDS
 
 /** A tracing context that will log the beginning and the end of all traces along with
   * their tags.
   * The traces will be emitted with a TRACE level, so make sure to configure your logging backend
   * to ennable the TRACE level for com.colisweb.tracing
   */
-class LoggingTracingContext[F[_]: Sync: Timer](
+class LoggingTracingContext[F[_]: Sync](
     traceIdP: String,
     spanIdP: String,
     tagsRef: Ref[F, Tags],
@@ -41,7 +39,7 @@ object LoggingTracingContext extends StrictLogging {
   /** Returns a Resource[F, TracingContext[F]]. The first log will be emitted
     * as the resource is acquired, the second log when it is released.
     */
-  def apply[F[_]: Sync: Timer](
+  def apply[F[_]: Sync](
       parentContext: Option[LoggingTracingContext[F]] = None,
       idGenerator: Option[F[String]] = None,
       slf4jLogger: org.slf4j.Logger = logger.underlying,
@@ -52,7 +50,7 @@ object LoggingTracingContext extends StrictLogging {
   ): TracingContextResource[F] =
     resource(parentContext, idGenerator, slf4jLogger, operationName, correlationId).evalMap(ctx => ctx.addTags(tags).map(_ => ctx))
 
-  private def resource[F[_]: Sync: Timer](
+  private def resource[F[_]: Sync](
       parentContext: Option[LoggingTracingContext[F]],
       idGenerator: Option[F[String]],
       slf4jLogger: org.slf4j.Logger,
@@ -68,7 +66,7 @@ object LoggingTracingContext extends StrictLogging {
       tagsRef <- Ref[F].of[Tags](Map.empty)
       spanId  <- idGeneratorValue
       traceId <- traceIdF
-      start   <- Clock[F].monotonic(MILLISECONDS)
+      start   <- Clock[F].monotonic.map(_.toMillis)
       ctx     = new LoggingTracingContext[F](traceId, spanId, tagsRef, correlationId)
       details = SpanDetails(start, traceId, spanId, ctx, tagsRef)
       _ <- logger.trace("Trace {} Starting Span {} ({})", traceId, spanId, operationName)
@@ -78,7 +76,7 @@ object LoggingTracingContext extends StrictLogging {
       case SpanDetails(start, traceId, spanId, _, tagsRef) =>
         for {
           tags <- tagsRef.get
-          end  <- Clock[F].monotonic(MILLISECONDS)
+          end  <- Clock[F].monotonic.map(_.toMillis)
           duration = end - start
           _ <- logger.trace(
             "Trace {} Finished Span {} ({}) in {}ms. Tags: {}",
@@ -109,7 +107,7 @@ object LoggingTracingContext extends StrictLogging {
     * This is provided for convenience and consistency with regards to the other
     * tracing contexts types.
     */
-  def builder[F[_]: Sync: Timer]: F[TracingContextBuilder[F]] =
+  def builder[F[_]: Sync]: F[TracingContextBuilder[F]] =
     Sync[F].delay((operationName: String, tags: Tags, correlationId: String) =>
       LoggingTracingContext.apply(correlationId = correlationId)(operationName, tags)
     )
