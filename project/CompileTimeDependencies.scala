48c30f64972aea4d4d9d43ed29279c6a6229bc21
Scala Steward <gitlab@colisweb.com>
2021-04-05 01:12:31 +0000

    Update scala-collection-compat to 2.4.3

--- project/CompileTimeDependencies.scala
+++ project/CompileTimeDependencies.scala
@@ -14,7 +14,7 @@ object Versions {
   final val log4cats        = "1.2.2"
   final val opentracing     = "0.33.0"
   final val requests        = "0.6.8"
-  final val scalaCompat     = "2.4.2"
+  final val scalaCompat     = "2.4.3"
   final val scalaLogging    = "3.9.3"
   final val scalatest       = "3.2.9"
   final val scalaCollection = "2.1.4"
