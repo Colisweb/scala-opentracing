22605503608812e9077e162d1f1471451ae4154a
Scala Steward <gitlab@colisweb.com>
2021-05-14 22:19:00 +0000

    Update cats-effect to 2.5.1

--- project/CompileTimeDependencies.scala
+++ project/CompileTimeDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Versions {
 
   final val cats            = "2.6.1"
-  final val catsEffect      = "2.3.3"
+  final val catsEffect      = "2.5.1"
   final val circe           = "0.13.0"
   final val datadog         = "0.68.0"
   final val fs2             = "4.0.0"
