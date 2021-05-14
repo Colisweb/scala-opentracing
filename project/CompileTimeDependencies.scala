4f174c00dfcda3bfe622699ca794346b1cb8c371
Scala Steward <gitlab@colisweb.com>
2021-05-14 22:16:05 +0000

    Update requests to 0.6.9

--- project/CompileTimeDependencies.scala
+++ project/CompileTimeDependencies.scala
@@ -13,7 +13,7 @@ object Versions {
   final val logstash        = "6.6"
   final val log4cats        = "1.2.2"
   final val opentracing     = "0.33.0"
-  final val requests        = "0.6.8"
+  final val requests        = "0.6.9"
   final val scalaCompat     = "2.4.4"
   final val scalaLogging    = "3.9.3"
   final val scalatest       = "3.2.9"
